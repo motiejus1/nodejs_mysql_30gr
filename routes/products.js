b795809ee40131e1c9c4edc5f7e1e6c403eaf33e
motiejus1 <motiejus.baliukonis@vka.lt>
2022-04-30 15:08:08 +0300

    *edit and update route

--- routes/products.js
+++ routes/products.js
@@ -96,5 +96,83 @@ router.get('/delete/(:productCode)', function(req,res,next) {
         }
     });
 });
+//tiktai uz vieno produkto informacijos atvaizdavima
+router.get('/edit/(:productCode)', function(req,res, next){
+    let productCode = req.params.productCode;
+
+    //select uzklausa
+    // SElect * FROM products where productCode = productCode
+
+    databaseConnection.query('SELECT * FROM products WHERE productCode="'+productCode+'"', function(err,rows, fields){
+        if(err) throw err;
+
+        if(rows.length <=0) {
+            req.flash('error', 'Toks produktas nerastas');
+            res.redirect('/products');
+        } else {
+            res.render('products/edit',{
+                productCode: rows[0].productCode ,
+                productName: rows[0].productName ,
+                productLine: rows[0].productLine ,
+                buyPrice: rows[0].buyPrice
+            });
+        }
+
+    });
+});  
+
+//produkto redagavimas
+
+router.post('/update/:productCode', function(req, res, next) {
+
+    //imamas is nuorodos
+    let productCode = req.params.productCode;
+    //redaguojami kintamieji yra imami is formos
+    let productName = req.body.productName;
+    let productLine = req.body.productLine;
+    let buyPrice = req.body.buyPrice;
+    let errors = false;
+
+    if(productName.length === 0 || productLine.length === 0 || buyPrice.length === 0) {
+        errors = true;
+        req.flash('error', 'Visi laukeliai turi buti uzpildyti');
+
+        res.render('products/edit', {
+            productCode: productCode ,
+            productName: productName,
+            productLine: productLine,
+            buyPrice: buyPrice
+        })
+    }
+
+    if(!errors) {
+        var form_data = {
+            productName: productName,
+            productLine: productLine,
+            buyPrice: buyPrice
+        }
+
+        databaseConnection.query('UPDATE productus SET ? WHERE productCode="'+productCode+'"', form_data, function(err, result){
+            if(err) {
+                req.flash('error', err);
+                res.render('products/edit', {
+                    productCode: productCode ,
+                    productName: form_data.productName,
+                    productLine: form_data.productLine,
+                    buyPrice: form_data.buyPrice
+                })
+                
+            } else {
+                req.flash('success', 'Produktas redaguotas sekmingai');
+                res.redirect('/products');
+            }
+
+        });
+
+    }
+
+
+});
+
 
 module.exports = router;
