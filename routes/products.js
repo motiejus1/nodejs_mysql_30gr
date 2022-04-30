04aaf6c6af378770a9dd7cabac3f294e81049bb9
motiejus1 <motiejus.baliukonis@vka.lt>
2022-04-30 13:40:51 +0300

    *index.ejs

--- routes/products.js
+++ routes/products.js
@@ -5,20 +5,19 @@
 // ...
 
 var express = require('express');
-const { DEC8_BIN } = require('mysql/lib/protocol/constants/charsets');
 var router = express.Router();
 var databaseConnection = require('../lib/database');
 
 // /products
 
-router.get('/products', function(req, res, next) {
+router.get('/', function(req, res, next) {
     //
     // /products -> pasirenka atitinkama funkcija kuria reikia ivykdti ir grazina atsakyma atvaizdavimui
     // /products kreipiasi i kontroleri, kontroleris kreipiasi i duomenu baze(modeli) ir grazina is modelio atsakyma
     databaseConnection.query('SELECT * FROM products', function(err, rows){
         if(err) {
-            req.flash('error', err);
-            res.render('products',{data: ''});
+            //req.flash('error', err);
+            res.render('products',{data: 'test'});
         } else {
             res.render('products',{data:rows}); //data:rows yra sql uzklausos rezultatas kaip JSON masyvas
         }
